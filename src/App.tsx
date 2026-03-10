20abf694db4c7261b933d5e25277df461bc98a39
Leonid Vuhalter <leo4vu@gmail.com>
2026-03-10 15:00:00 +0100

    Complete homework 1 and 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import "./App.css";
 import Greeting from "./components/Greeting/Greeting";
 import Hero from "./components/Hero/Hero";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
+import Tool from "./components/Tool/Tool";
 
 function App() {
   return (
     <>
       <h1>Hello React</h1>
+      <Tool />
 
       <Greeting name="John" />
       <Greeting name="Bob" />
       <Greeting name="Ioann" age={20} />
       <Hero />
+      <ProfileCard
+        avatar="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+        name="Leonid"
+        description="Frontend Developer in training"
+      />
     </>
   );
 }
---
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,21 @@
+import styles from "./ProfileCard.module.css";
+
+interface Props {
+  avatar: string;
+  name: string;
+  description: string;
+}
+
+export default function ProfileCard(props: Props) {
+
+  const { avatar, name, description } = props;
+
+  return (
+  
+    <div className={styles["profile-card"]}>
+      <img src={avatar} alt="User avatar" />
+      <h2>{name}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
