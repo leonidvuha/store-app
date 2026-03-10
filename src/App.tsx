4ce888970344e11fb0cbdea984cdda397ed0d84d
Leonid Vuhalter <leo4vu@gmail.com>
2026-03-10 12:26:38 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Greeting from "./components/Greeting/Greeting";
+import Hero from "./components/Hero/Hero";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Greeting />
+      <Greeting />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
+
